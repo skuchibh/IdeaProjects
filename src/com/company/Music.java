a845d61826a0d98150e16b0385028cd50f463be8
skuchibh <suryavamsi.gitamuniv@gmail.com>
2017-11-13 18:47:48 +0530

    Commit 1st time

--- src/com/company/Music.java
+++ src/com/company/Music.java
@@ -5,7 +5,7 @@ import java.io.*;
 public class Music {
 
         public static void main(String[] args) throws IOException {
-
+            System.out.println("HI");
             try(PrintWriter pw=new PrintWriter(new OutputStreamWriter(System.out))){
                 pw.println("FF");
             }
